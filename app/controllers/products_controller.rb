954d1c85db25555d00519018fd2f5aa8a1c63e6b
Ali <mino.ict@gmail.com>
2019-05-01 07:13:06 +0200

    Add images to Product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -72,7 +72,7 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:price, :quantity, :title, :description)
+      params.require(:product).permit(:price, :quantity, :title, :description, images: [])
     end
 
     def product_associatives(product, parameters)
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
     belongs_to :brand
     belongs_to :category
     belongs_to :user    # seller
+    has_many_attached :images
 
     validates :title, :presence => true, length: { minimum: 5 }
     validates :description, :presence => true, length: { minimum: 10 }
@@ -17,5 +18,17 @@ class Product < ApplicationRecord
             if user.role != 'seller'
                 errors.add(:product, 'can be added only by sellers')
             end 
+        end
+
+        def images_types
+            if images.attached?
+                images.each do |image|
+                    unless image.content_type.in?(%('image/jpeg image/png'))
+                        errors.add(:images, "have not supported format, only JPEG and PNG are supported")
+                    end
+                end
+            else
+                errors.add(:images, "are missing")
+            end
         end
 end
