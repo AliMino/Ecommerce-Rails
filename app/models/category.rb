4221d61e0d1d2797354266ea83da4e7f0ac006c1
Ali <mino.ict@gmail.com>
2019-05-01 06:19:16 +0200

    Add categories controller & Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,9 @@
 class Category < ApplicationRecord
+    validates :name, :presence => true, :uniqueness => true
+    has_many :products, dependent: :destroy
+
+    def self.get_by_name(category_name)
+        category_name = category_name.capitalize.pluralize
+        Category.where(:name => category_name).limit(1)[0]
+    end
 end
