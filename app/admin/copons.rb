1a3dab35a97dc424c2d4facda5f041b3fc750d37
EsraaKedwany <esraa.mohamed1006@gmail.com>
2019-05-02 19:20:17 +0200

    Add permit_params to coupon, store

--- app/admin/copons.rb
+++ app/admin/copons.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Copon do
+    permit_params :has_fixed_amount, :discount, :expiration_type
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
--- app/admin/stores.rb
+++ app/admin/stores.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Store do
+    permit_params :name, :summary, :user_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
