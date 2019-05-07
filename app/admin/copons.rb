dff88503129084bf1ee124f6de8f263aaf282ea3
EsraaKedwany <esraa.mohamed1006@gmail.com>
2019-05-07 23:41:51 +0200

    Add expiration types to admin

--- app/admin/copons.rb
+++ app/admin/copons.rb
@@ -1,5 +1,40 @@
 ActiveAdmin.register Copon do
-    permit_params :has_fixed_amount, :discount, :expiration_type
+    permit_params :product_id, :has_fixed_amount, :discount, :expiration_type, :associative
+
+    # form do |f|
+    #     f.inputs 'copons' do
+    #         # f.label :product
+    #         # f.select (:product) do
+    #         #     products_array = Product.get_products_with_no_copons(copon.product).collect { |p| [p.title + " - " + p.category.name + " by " + p.brand.name, p.id] }
+    #         #     if copon.product
+    #         #         options_for_select(products_array, :selected => copon.product.id)
+    #         #     else
+    #         #         options_for_select(products_array)
+    #         #     end
+    #         # end
+    #         f.label :has_fixed_amount
+    #         f.check_box :has_fixed_amount
+    #         f.input :discount
+    #         f.select :Expiration type, [0,1]
+    #     end
+    # end
+
+#     <%= form_for(transaction) do |f| %> 
+
+#    <div class="field">
+#     <%= f.label :transactionType %>
+#     <%= f.select :transactionType, ['Income', 'Expense']%>
+#    </div>
+#      <!--  here comes ternary operator -->    
+#    <div class="field <%= transaction.transactionType == 'Income' ? 'present' : 'hidden' %>">
+#     <%= f.label :amount %>
+#     <%= f.number_field :amount, options = { max: 0 }%>
+#    </div>
+#    <div class="field <%= transaction.transactionType != 'Income' ? 'present' : 'hidden' %>"">
+#     <%= f.label :amount %>
+#     <%= f.number_field :amount, options = { min: 0 }%>
+#    </div>
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
