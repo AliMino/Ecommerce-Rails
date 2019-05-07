e192fa308ae79a7bcadcde2380992e77bb482408
Ali <mino.ict@gmail.com>
2019-05-07 11:17:26 +0200

    Add UsedCopon model

--- app/models/used_copon.rb
+++ app/models/used_copon.rb
@@ -1,2 +1,8 @@
 class UsedCopon < ApplicationRecord
+    belongs_to :user
+    belongs_to :copon
+
+    def self.get_usage_for_copon(copon_id)
+        UsedCopon.where("copon_id", copon_id)
+    end
 end
