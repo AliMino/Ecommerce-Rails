73f3f643d57d2b9580d8106b6ed5b136dc8d842c
EsraaKedwany <esraa.mohamed1006@gmail.com>
2019-05-07 21:27:12 +0200

    Assign a user to a store as a seller

--- app/admin/stores.rb
+++ app/admin/stores.rb
@@ -1,5 +1,22 @@
 ActiveAdmin.register Store do
     permit_params :name, :summary, :user_id
+
+    controller do
+        def create
+            @store = Store.new(permitted_params[:store])
+            @user=@store.user
+            @user.role=1
+            @user.save
+
+            if @store.save
+                flash[:notice] = "Store was successfully created!"
+                render :index
+            else
+                render :new
+            end
+            
+        end
+    end
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
