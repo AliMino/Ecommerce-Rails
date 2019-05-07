8d3181b2422f874f41b1a0c1c82a699f6d49d6cd
Ali <mino.ict@gmail.com>
2019-05-07 11:16:49 +0200

    Add methods to limit the usage of th copon

--- app/models/copon.rb
+++ app/models/copon.rb
@@ -3,6 +3,10 @@ class Copon < ApplicationRecord
     validates :discount, presence: true
     belongs_to :product
 
+    def self.get_by_id(id)
+        Copon.find(id)
+    end
+
     def get_symbol
         if has_fixed_amount
             "$"
@@ -21,10 +25,18 @@ class Copon < ApplicationRecord
 
     def is_expired
         if expiration_type == "usage"
-            false
+            get_usage.length == get_expiration
         else
             get_expiration < DateTime.now
         end
+    end
+
+    def get_usage
+        UsedCopon.get_usage_for_copon(self.id)
+    end
+
+    def is_available_for(user_id)
+        UsedCopon.where("copon_id = ? and user_id = ?", self.id, user_id).length == 0
     end
 
 end
